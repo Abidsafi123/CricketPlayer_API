18936d3346dff3ad58d3105e20b4820e11caca75
= <=>
2025-05-19 08:59:57 -0700

    inital commit

--- src/index.js
+++ src/index.js
@@ -13,68 +13,14 @@ connection();
 
 
 app.use(express.json())
-app.use("/",router)
+app.use("/api",router)
+ 
 
 
 
 const port =  3001;
-//  app.post("/player",async(req,res)=>{
-//     try{
-// const insertPlayer = new ranking(req.body);
-// const savedPlayer = await insertPlayer.save();
-// res.status(201).send(savedPlayer);
-//     }catch(err){
-//         res.status(500).send(`error while postin data`+err);
-
-//     }
-//  })
-//  app.get("/player",async(req,res)=>{
-//     try{
-//         const findPlayer = await ranking.find().sort({ranking:1});
-//         const playerLength = await ranking.countDocuments();
-//         console.log(`we have ${playerLength} number of player in our DataBase`)
-       
-//         res.status(201).send(findPlayer);
-//         res.send(playerLength)
-
-       
-//     }catch(err){
-//         res.status(500).send(err);
-//     }
-//  })
-//  app.get("/player/:id",async(req,res)=>{
-//     try{
-//         const _id = req.params.id;
-//         const findOne = await ranking.findById(_id);
-//         res.status(201).send(findOne);
-
-//     }catch(err){
-//         res.status(500).send("internal server error"+err);
-//     }
-//  })
-//  //UPDATE BY ID
-//  app.patch("/player/:id",async(req,res)=>{
-//     try{
-//         const _id = req.params.id;
-//         const updataPlayer = await ranking.findByIdAndUpdate(_id,req.body,{
-//             new:true
-//         })
-//         res.status(201).send(`player updated successfylly`+updataPlayer)
-//     }catch(err){
-//         res.status(500).send("internal server error",err);
-//     }
-//  })
-//  //delete 
-//  app.delete("/player/:id",async(req,res)=>{
-//     try{
-// const _id = req.params.id;
-//         const deletePlayer = await ranking.findByIdAndDelete(_id);
-//         res.status(201).send("player deleted successfully"+deletePlayer);
-//     }catch(err){
-
-//     }res.status(404).send("player deleted"+deletePlayer);
-//  })
-
+ 
+  
 
 app.listen(port,()=>{
    console.log(`app is running on localhost:${port}`);
